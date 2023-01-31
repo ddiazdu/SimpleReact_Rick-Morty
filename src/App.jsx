b9275e9a066e8c42dff8a5af056a094bed05f075
NaicoQ <diego.diazdu@gmail.com>
2023-01-30 23:33:31 -0300

    fix #05 corrigiendo tamaño de imagenes

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,9 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { Pagination } from "react-bootstrap";
 
-
-
 function App() {
 
-  const [personajes, setPersonajes] = useState(null);
+  const [personajes, setPersonajes] = useState([]);
   const [infoPage, setInfoPage] = useState({});
   const [itemPagination, setItemPagination] = useState([]);
 
@@ -18,7 +16,7 @@ function App() {
     setPersonajes(getData.data.results);
     //Seteando EpisodesCount, NextPage, Pages, Prev
     setInfoPage(getData.data.info);
-    console.log(getData.data.info)
+
 
   };
 
@@ -35,7 +33,7 @@ function App() {
       items.push(
 
         <Pagination.Item
-          className="bg-cyan-600 hover:bg-cyan-900 p-5 mb-5 rounded-lg shadow-md text-white font-bold text-xl"
+          className="cursor bg-cyan-500 hover:bg-cyan-600 p-5 mb-5 rounded-lg shadow-md text-white font-bold text-xl"
           key={i}
           onClick={(e) => (getPersonajes(parseInt(e.target.text)))}>{i}
 
@@ -45,28 +43,35 @@ function App() {
     setItemPagination(items)
   }, [infoPage]);
 
-
   return (
+
     <div>
+
       <nav className="bg-slate-500 p-12 mb-5">
         <div>
           <p className="titulo_app text-center">Rick and Morty API</p>
         </div>
       </nav>
+
+      <div className="flex justify-center">
+
+      </div>
       <div className="grid md:grid-cols-4 gap-5 p-10">
         {/* Condicional que evalua si el state está en null */}
-        {personajes != null
+        {personajes != []
           ? /* Recorro los personajes con una variable temporal */
           personajes.map((personaje) => (
             <div
-              className="text-center rounded shadow-lg bg-gray-50 p-5"
               key={personaje.id}
+              className="text-center rounded shadow-lg bg-gray-50 p-5"
+
             >
               <img
-                className="img-100 rounded-full mb-2"
+                className="img-100 rounded-full mb-2 mx-auto"
                 src={personaje.image}
                 alt="Imagen del personaje"
               />
+
               <p className="text-2xl font-black">Name: {personaje.name}</p>
               <p className="text-lg">Genre: {personaje.gender}</p>
               <p className="text-lg">Species: {personaje.species}</p>
@@ -84,21 +89,21 @@ function App() {
                 {personaje.status}
               </p>
               <p>Origin: {personaje.origin.name}</p>
+
             </div>
           ))
           : //En caso de que el state esté en Null, esta será la respuesta por defecto
           "No hay personajes"}
+
       </div>
 
-      <Pagination className="flex space-x-1 overflow-x-scroll">
+      <Pagination className="flex space-x-1 mx-5 overflow-x-scroll">
 
         {itemPagination.map(item => (
           item
         ))}
 
       </Pagination>
-
-
 
 
     </div>
